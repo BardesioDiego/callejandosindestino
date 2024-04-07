cae015ad5dc50d3971a276e3b375d4df4137d65e
Diego Bardesio <diegobardesio@gmail.com>
2024-04-07 00:08:55 -0300

    Ultimos del dia

--- js/main.js
+++ js/main.js
@@ -73,36 +73,7 @@
                 items:2
             },
             992:{
-                items:3
-            }
-        }
-    });
-
-
-    // Product carousel
-    $(".product-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
                 items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
             }
         }
     });
